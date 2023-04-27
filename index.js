39e2820e08d812e4210aa3edf5a5579035d6ad83
HoffmasterDevin <118146567+HoffmasterDevin@users.noreply.github.com>
2023-04-27 13:43:06 -0500

    added logic fog markdown generation

--- index.js
+++ index.js
@@ -16,11 +16,90 @@ const inputs = [
     "What is your email address?: ",
 ];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) {
+            console.error(err)
+        } else {
+            console.log('Success: README File Generated!')
+        }
+    });
+}
 
-// TODO: Create a function to initialize app
-function init() {}
+// Function to initialize app
+function init() {
+    inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: questions[0],
+            name: 'title',
+        },
+        {
+            type: 'input',
+            message: questions[1],
+            name: 'description',
+        },
+        {
+            type: 'input',
+            message: questions[2],
+            name: 'install',
+        },
+        {
+            type: 'input',
+            message: questions[3],
+            name: 'usage',
+        },
+        {
+            type: 'input',
+            message: questions[4],
+            name: 'contribution',
+        },
+        {
+            type: 'input',
+            message: questions[5],
+            name: 'testing',
+        },
+        {
+            type: 'list',
+            message: questions[6],
+            name: 'license',
+            choices: [
+                "None",
+                "MIT License",
+                "Apache License 2.0",
+                "GNU General Public License v3.0",
+            ]
+        },
+        {
+            type: 'input',
+            message: questions[7],
+            name: 'gitHubUser',
+        },
+        {
+            type: 'input',
+            message: questions[8],
+            name: 'email',
+        },
+    ])
+    .then((response) => {
+        const prompt = {
+            title: response.title,
+            description: response.description,
+            install: response.install,
+            usage: response.usage,
+            contribution: response.contribution,
+            testing: response.testing,
+            license: response.license,
+            gitHubUser: response.gitHubUser,
+            email: response.email,
+        }
+
+        const readmeText = markdown.generateMarkdown(prompt);
+        writeToFile(readmeFilePath, readmeText);
+    });
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,82 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// Returns a license badge based on which license is passed in
+function renderLicenseBadge(license) {
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+  let badge;
+  switch(license){
+    case 'MIT License':
+      badge = '![MIT](https://img.shields.io/badge/license-MIT-informational)'
+      break;
+    case 'Apache License 2.0':
+      badge = '![Apache](https://img.shields.io/badge/license-Apache-informational)'
+      break;
+    case 'GNU General Public License v3.0':
+      badge = '![GNU](https://img.shields.io/badge/license-GNU-informational)'
+      break;
+    default:
+      badge = '';
+  }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+  return badge;
 }
 
-module.exports = generateMarkdown;
+// Returns the license link
+function renderLicenseSection(license) {
+
+  let link;
+  
+  switch(license){
+    case 'MIT License':
+      link = '[MIT License](https://mit-license.org/)'
+      break;
+    case 'Apache License 2.0':
+      link = '[Apache License 2.0](https://www.apache.org/licenses/LICENSE-2.0.txt)'
+      break;
+    case 'GNU General Public License v3.0':
+      link = '[GNU General Public License v3.0](https://www.gnu.org/licenses/gpl-3.0.txt)'
+      break;
+    default:
+      link = '';
+  }
+
+  if (link === ''){
+    return 'The application has no license.';
+  }else{
+    return `This application is licenced by ${link}`;
+  }
+  
+}
+
+// Generate markdown for README
+function generateMarkdown(data) {
+
+  const badge = renderLicenseBadge(data.license);
+  const licenseSection = renderLicenseSection(data.license);
+
+  return `# ${data.title}
+  ${badge}
+  ## Description
+  ${data.description}
+  ## Table of Contents
+  1. [Installation](#installation)
+  2. [Usage](#usage)
+  3. [Contributing](#contributing)
+  4. [Tests](#tests)
+  5. [License](#license)
+  6. [Questions](#questions)
+  ## Installation
+  ${data.install}
+  ## Usage
+  ${data.usage}
+  ## Contributing
+  ${data.contribution}
+  ## Tests
+  ${data.testing}
+  ## License
+  ${licenseSection}
+  ## Questions
+  This application was created by [${data.gitHubUser}](https://github.com/${data.gitHubUser}). <br>
+  If you have any questions, they can be reached at ${data.email}.
+  `;
+}
+
+module.exports = { generateMarkdown };
